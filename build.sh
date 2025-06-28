019c0e2e115573f3d2baa20d37cd89fa8cda9e2f
Jason Erasmus <jason.erasmus@live.co.za>
2025-06-28 21:24:38 +0100

    testing build and docker container int

--- build.sh
+++ build.sh
@@ -17,17 +17,19 @@ CLONE_DIR="app-temp"
 echo "Cloning the repository..."
 rm -rf $CLONE_DIR
 git clone $REPO_URL $CLONE_DIR
+
+
 cd $CLONE_DIR
 
 echo "Building Docker image..."
 docker compose build
 
 echo "Running Django migrations..."
-docker compose run django python manage.py migrate
+docker compose run backend python manage.py migrate
 
 
 echo "Starting application..."
 docker compose up -d
 
-echo "Done! Visit http://localhost:3000"
+echo "Done! Visit http://localhost:3001"
 
